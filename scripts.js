86259d4470621462303d32370c3cb6607a813fd7
thesadriger <thesadriger@gmail.com>
2024-07-09 21:29:22 +0300

    Изменения
    Обработчики событий: Добавлены обработчики событий для открытия и закрытия модальных окон.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,19 @@
 window.onload = function() {
-    // Get modal windows and close buttons
     var modals = document.querySelectorAll(".modal");
     var closeButtons = document.querySelectorAll(".close");
 
-    // Show the first modal window when the page loads
     if (modals.length > 0) {
         modals[0].style.display = "block";
     }
 
-    // Close the modal window when clicking the close button
     closeButtons.forEach(function(button) {
         button.addEventListener("click", function(e) {
-            e.stopPropagation(); // Prevent closing when clicking the button
-            var currentModal = this.closest(".modal"); // Find the nearest modal window
+            e.stopPropagation();
+            var currentModal = this.closest(".modal");
             closeModal(currentModal.id);
         });
     });
 
-    // Functions to open and close modal windows
     function openModal(modalId) {
         var modal = document.getElementById(modalId);
         if (modal) {
@@ -28,11 +24,10 @@ window.onload = function() {
     function closeModal(modalId) {
         var modal = document.getElementById(modalId);
         if (modal) {
-            modal.style.display = "none"; // Close the modal window
+            modal.style.display = "none";
         }
     }
 
-    // Add event handlers for navigation buttons
     document.querySelectorAll('.nav-button-friends, .nav-button-mining, .nav-button-tasks').forEach(function(button) {
         button.addEventListener("click", function() {
             var modalId;
@@ -47,12 +42,11 @@ window.onload = function() {
         });
     });
 
-    // Close the modal window when clicking outside its area
     window.addEventListener("click", function(event) {
         var clickedElement = event.target;
         modals.forEach(function(modal) {
             if (modal.style.display === "block" && !modal.contains(clickedElement) && !clickedElement.classList.contains('nav-button-friends') && !clickedElement.classList.contains('nav-button-mining') && !clickedElement.classList.contains('nav-button-tasks')) {
-                closeModal(modal.id); // If click outside the modal window, close it
+                closeModal(modal.id);
             }
         });
     });
