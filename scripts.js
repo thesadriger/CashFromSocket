5e518a60ca0922f40695ed79e4d7c32dc0e33681
thesadriger <64961875+thesadriger@users.noreply.github.com>
2024-07-09 12:34:52 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('mineButton').addEventListener('click', function() {
+    // Здесь должна быть логика для взаимодействия с сервером
+    // Например, отправка запроса на майнинг и обновление баланса
+});
