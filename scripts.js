f18e9b704fa639d79e1a43ad4d919c91b1ab6ed3
thesadriger <thesadriger@gmail.com>
2024-07-09 13:10:13 +0300

    1 этап

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
-document.getElementById('mineButton').addEventListener('click', function() {
-    // Здесь должна быть логика для взаимодействия с сервером
-    // Например, отправка запроса на майнинг и обновление баланса
-});
+window.onload = function() {
+    var modal = document.getElementById("balanceModal");
+    var span = document.getElementsByClassName("close")[0];
+    
+    // Показываем модальное окно при загрузке страницы
+    modal.style.display = "block";
+    
+    // Закрываем модальное окно при закрытии
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+};
